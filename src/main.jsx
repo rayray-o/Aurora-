d744a881a1fd790de9d6695718799b6b1421fefd
Ray <d2arknotfound@gmail.com>
2026-08-24 18:42:30 +0530

    Wrap App component with AuthProvider and AuthBridge

--- src/main.jsx
+++ src/main.jsx
@@ -1,10 +1,19 @@
 import React from "react";
 import ReactDOM from "react-dom/client";
+
 import App from "./App";
 import "./index.css";
 
-ReactDOM.createRoot(document.getElementById("root")).render(
+import { AuthProvider } from "./auth/AuthProvider";
+import AuthBridge from "./auth/AuthBridge";
+
+ReactDOM.createRoot(
+  document.getElementById("root")
+).render(
   <React.StrictMode>
-    <App />
+    <AuthProvider>
+      <App />
+      <AuthBridge />
+    </AuthProvider>
   </React.StrictMode>
 );
